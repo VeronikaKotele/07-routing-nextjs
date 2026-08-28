6e8c1084893a3a9ad604551826f4ee60173905e4
Veronika Kotelevska <babychveronika@gmail.com>
2026-08-28 08:55:33 +0200

    move error from app\notes to app\notes\filter\[...slug]

--- app/notes/filter/[...slug]/error.tsx
+++ app/notes/filter/[...slug]/error.tsx
@@ -8,7 +8,7 @@ type Props = {
 const Error = ({ error, reset }: Props) => {
     return (
         <div>
-            <p>Could not fetch note details. {error.message}</p>
+            <p>Could not fetch the list of notes. {error.message}</p>
             <button onClick={reset}>Try again</button>
         </div>
     );
